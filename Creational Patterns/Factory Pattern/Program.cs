88c8ea3bcbd813573103c09c52bcfb8f38badad1
Himanshu Jain <himanshu.jain@milliman.com>
2025-07-30 13:43:09 +0530

    More factory pattern example.

--- Creational Patterns/Factory Pattern/Program.cs
+++ Creational Patterns/Factory Pattern/Program.cs
@@ -11,14 +11,140 @@
             //ReportGenerator report = new ReportGenerator(logger);
             //report.Generate();
 
-            NotificationFactory notificationFactory = new EmailNotificationFactory();
-            var emailNotification = notificationFactory.NotificationCreator();
+            //NotificationFactory notificationFactory = new EmailNotificationFactory();
+            //var emailNotification = notificationFactory.NotificationCreator();
 
 
-            PDfGenerator pDfGenerator = new PDfGenerator(emailNotification);
-            pDfGenerator.Generate();
+            //PDfGenerator pDfGenerator = new PDfGenerator(emailNotification);
+            //pDfGenerator.Generate();
+
+            Restaurant restaurant = new Restaurant();
+            restaurant.Order("AdrakChai", "Burger");
         }
     }
+
+    #region Restaurant
+    //Client Code
+    public class Restaurant
+    {
+        public Restaurant()
+        {
+        }
+
+        public void Order(string teaType, string snackType)
+        {
+            IChai chai = Simulator.ChaiSimulator(teaType);
+            chai.Banao();
+            ISnacks snack = Simulator.SnacksSimulator(snackType);
+            snack.CreateSnacks();
+        }
+    }
+
+    public class Simulator {
+        public static IChai ChaiSimulator(string teaType)
+        {
+            return teaType switch
+            {
+                "AdrakChai" => new AdrakChaiFactory().ChaiCreattor(),
+                "EliachiChai" => new EliachiChaiFactory().ChaiCreattor(),
+                _ => throw new NotSupportedException($"Tea type '{teaType}' is not supported.")
+            };
+        }
+
+        public static ISnacks SnacksSimulator(string snackType)
+        {
+            return snackType switch
+            {
+                "Burger" => new BurgerSnacksFactory().CreateSnacks(),
+                "Pizza" => new PizzaSnacksFactory().CreateSnacks(),
+                _ => throw new NotSupportedException($"snack type '{snackType}' is not supported.")
+            };
+        }
+    }
+
+    //Products
+    public interface IChai
+    {
+        void Banao();
+    }
+    public interface ISnacks
+    {
+        void CreateSnacks();
+    }
+
+    //Product Concret
+    public class AdrakChai : IChai
+    {
+        public void Banao() {
+            Console.WriteLine("Adrak Chai ban gayi hai");
+        }
+    }
+
+    public class EliachiChai : IChai
+    {
+        public void Banao()
+        {
+            Console.WriteLine("Eliachi Chai ban gayi hai");
+        }
+    }
+    public class BurgerSnacks: ISnacks{
+        public void CreateSnacks()
+        {
+            Console.WriteLine("Burger is created");
+        }
+    }
+    public class PizzaSnacks : ISnacks
+    {
+        public void CreateSnacks()
+        {
+            Console.WriteLine("Pizza is created");
+        }
+    }
+
+
+    //Creator
+    public abstract class SnacksFactory
+    {
+        public abstract ISnacks CreateSnacks();
+    }
+
+    public abstract class ChaiFactory
+    {
+        public abstract IChai ChaiCreattor();
+    }
+
+    //Creator Concrete
+    public class BurgerSnacksFactory : SnacksFactory
+    {
+        public override ISnacks CreateSnacks()
+        {
+            return new BurgerSnacks();
+        }
+    }
+    public class PizzaSnacksFactory : SnacksFactory
+    {
+        public override ISnacks CreateSnacks()
+        {
+            return new PizzaSnacks();
+        }
+    }
+
+    public class AdrakChaiFactory : ChaiFactory {
+        public override IChai ChaiCreattor() { 
+            return new AdrakChai();
+        }
+    }
+
+    public class EliachiChaiFactory : ChaiFactory
+    {
+        public override IChai ChaiCreattor()
+        {
+            return new EliachiChai();
+        }
+    }
+
+
+    #endregion
 
     #region Notification System
 
